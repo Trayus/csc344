066e71a628e523c57daacd5da55d2b888af96ecc
Tim Adam <tiadam@calpoly.edu>
2014-01-22 22:40:02 -0800

    project 2

---
+++ project2(jasmid)/code.js
@@ -0,0 +1,111 @@
+var vec2 = function(x, y)
+{
+    this.x = x;
+	this.y = y;
+}
+wave_pts = [new vec2(0, 0), new vec2(0.15, 0.5), new vec2(0.3, 1), 
+	new vec2(0.4, 1), new vec2(0.48, 1), new vec2(0.5, 0), new vec2(0.52, -1), 
+	new vec2(0.6, -1), new vec2(0.7, -1), new vec2(0.85, -0.5), new vec2(1, 0)];
+env_pts = [new vec2(0, 0), new vec2(0.2, 1), new vec2(0.4, 0.5), 
+	new vec2(0.8, 0.5), new vec2(1, 0)];
+freq = 3;
+amp = 5;
+	
+function randomize()
+{
+	for (var i = 1; i < wave_pts.length - 1; i++)
+	{
+		wave_pts[i].x = Math.random() * (0.25 - i * 0.02) + wave_pts[i - 1].x;
+		if (wave_pts[i].x > 1) wave_pts[i].x = 1;
+		wave_pts[i].y = Math.random() * 2 - 1;
+	}
+	var lowest = 1, highest = -1, hi = 0, li = 0;
+	for (var i = 1; i < wave_pts.length - 1; i++)
+	{
+		if (wave_pts[i].y > highest)
+		{ highest = wave_pts[i].y; hi = i; }
+		if (wave_pts[i].y < lowest)
+		{ lowest = wave_pts[i].y; li = i; }
+	}
+	wave_pts[hi].y = 1;
+	wave_pts[li].y = -1;
+	
+	env_pts[2].y = Math.sqrt(Math.random());
+	env_pts[3].y = env_pts[2].y;
+	env_pts[1].x = Math.random() * 0.3;
+	env_pts[2].x = Math.random() * 0.2 + env_pts[1].x;
+	env_pts[3].x = Math.random() * 0.4 + 0.6;
+	
+	var f = document.getElementById("freq");
+	freq = Math.floor(Math.random() * 8);
+	f.innerHTML = "Sine-wave LFO Freq = " + freq + " Hz";
+	
+	var a = document.getElementById("amp");
+	amp = Math.floor(Math.random() * 12) + 3;
+	a.innerHTML = "Sine-wave LFO Freq = " + amp + "%";
+	
+	clearCanvases();
+}
+
+function clearCanvases()
+{
+	updateCanvas("wave");
+	updateCanvas("env");
+}
+
+function getCx(vec)
+{
+	return vec.x * 400 + 5;
+}
+function getCy(vec)
+{
+	return vec.y * -90 + 100;
+}
+
+function updateCanvas(name)
+{
+	var canvas = document.getElementById(name);
+	var context = canvas.getContext("2d");
+		
+	context.strokeStyle = "#ACC";
+	context.fillStyle = "#ACC";
+	context.fillRect(0,0,410,200);
+	
+	context.stroke();
+	
+	context.beginPath();
+	context.strokeStyle = "#444";
+	context.moveTo(5,5);
+	context.lineTo(5, name == "env"? 100 : 195);
+	context.moveTo(5,100);
+	context.lineTo(405,100);
+	context.stroke();
+	context.closePath();
+	
+	if (name == "wave")
+	{
+		context.beginPath();
+		context.strokeStyle = "#22F";
+		context.moveTo(getCx(wave_pts[0]), getCy(wave_pts[0])); 
+		console.log(wave_pts);
+		for (var i = 1; i < wave_pts.length; i++)
+		{
+			context.lineTo(getCx(wave_pts[i]), getCy(wave_pts[i])); 
+		}
+		context.stroke();
+		context.closePath();
+	}
+	else
+	{
+		context.beginPath();
+		context.strokeStyle = "#292";
+		context.moveTo(getCx(env_pts[0]), getCy(env_pts[0])); 
+		console.log(env_pts);
+		for (var i = 1; i < env_pts.length; i++)
+		{
+			context.lineTo(getCx(env_pts[i]), getCy(env_pts[i])); 
+		}
+		context.stroke();
+		context.closePath();
+	}
+}
---
+++ project2(jasmid)/synth.js
@@ -0,0 +1,269 @@
+
+function findInterpedOsc(t, spc)
+{
+	var ndx = (t % spc) / spc;
+	var bf = -1;
+	for (var i = 0; i < wave_pts.length - 1; i++)
+	{
+		if (ndx < wave_pts[i].x)
+		{
+			bf = i;
+			break;
+		}
+	}
+	if (bf == -1) return wave_pts[wave_pts.length - 1].y;
+	else
+	{
+		var interp = (ndx - wave_pts[i].x) / (wave_pts[i + 1].x - wave_pts[i].x);
+		return (1 - interp) * wave_pts[i].y + interp * wave_pts[i + 1].y;
+	}
+}
+
+var ENV_LEN = 20000;
+
+function applyEnvelope(result, t, spc, fade)
+{
+	if (fade != -1 && t > ENV_LEN * env_pts[2].x)
+	{
+		// fade
+		var interp = (fade - ENV_LEN * env_pts[3].x) / (1 - env_pts[3].x);
+		return result * ((1 - interp) * env_pts[3].y);
+	}
+	else if (t < ENV_LEN * env_pts[2].x)
+	{
+		if (t < ENV_LEN * env_pts[1].x)
+		{
+			var interp = t / env_pts[1].x;
+			return result * interp * env_pts[1].y;
+		}
+		else
+		{
+			var interp = (t - ENV_LEN * env_pts[1].x) / (env_pts[2].x - env_pts[1].x);
+			return result * ((1 - interp) * env_pts[1].y + interp * env_pts[2].y);
+		}
+	}
+	else
+	{
+		// sustain
+		return result * env_pts[3].y;
+	}
+}
+
+function applyLFO(result, t, sampleRate)
+{
+	var spc = sampleRate / freq; // global freq is LFO freq
+	var ndx = (t % spc) / spc;
+	
+	return result * (1 + (amp / 100) * Math.sin(ndx));
+}
+
+/** awesome takes in the current GUI params set **/
+function AwesomeGenerator (freq)
+{
+	var self = {'alive': true};
+	var spc = sampleRate / freq;
+	var t = 0;
+	var fading = -1;
+	
+	self.generate = function(buf, offset, count) {
+		for (; count; count--) {
+			var result = findInterpedOsc(t, spc);
+			result = applyEnvelope(result, t, spc, fading);
+			if (fading != -1 && result < 0.01)
+				self.alive = false;
+			else if (fading != -1)
+				fading++;
+			result = applyLFO(result, t, spc);
+			result *= 0.3;
+			buf[offset++] += result;
+			buf[offset++] += result;
+			t++;
+		}
+	}
+	
+	self.noteOff = function() {
+		fading = 0;
+	}
+	
+	return self;
+}
+
+
+function SineGenerator(freq) {
+	var self = {'alive': true};
+	var period = sampleRate / freq;
+	var t = 0;
+	
+	self.generate = function(buf, offset, count) {
+		for (; count; count--) {
+			var phase = t / period;
+			var result = Math.sin(phase * 2 * Math.PI) / 4;
+			buf[offset++] += result;
+			buf[offset++] += result;
+			t++;
+		}
+	}
+	
+	self.noteOff = function() {
+		self.alive = false;
+	}
+	
+	return self;
+}
+
+function ADSRGenerator(child, attackAmplitude, sustainAmplitude, attackTimeS, decayTimeS, releaseTimeS) {
+	var self = {'alive': true}
+	var attackTime = sampleRate * attackTimeS;
+	var decayTime = sampleRate * (attackTimeS + decayTimeS);
+	var decayRate = (attackAmplitude - sustainAmplitude) / (decayTime - attackTime);
+	var releaseTime = null; /* not known yet */
+	var endTime = null; /* not known yet */
+	var releaseRate = sustainAmplitude / (sampleRate * releaseTimeS);
+	var t = 0;
+	
+	self.noteOff = function() {
+		if (self.released) return;
+		releaseTime = t;
+		self.released = true;
+		endTime = releaseTime + sampleRate * releaseTimeS;
+	}
+	
+	self.generate = function(buf, offset, count) {
+		if (!self.alive) return;
+		var input = new Array(count * 2);
+		for (var i = 0; i < count*2; i++) {
+			input[i] = 0;
+		}
+		child.generate(input, 0, count);
+		
+		childOffset = 0;
+		while(count) {
+			if (releaseTime != null) {
+				if (t < endTime) {
+					/* release */
+					while(count && t < endTime) {
+						var ampl = sustainAmplitude - releaseRate * (t - releaseTime);
+						buf[offset++] += input[childOffset++] * ampl;
+						buf[offset++] += input[childOffset++] * ampl;
+						t++;
+						count--;
+					}
+				} else {
+					/* dead */
+					self.alive = false;
+					return;
+				}
+			} else if (t < attackTime) {
+				/* attack */
+				while(count && t < attackTime) {
+					var ampl = attackAmplitude * t / attackTime;
+					buf[offset++] += input[childOffset++] * ampl;
+					buf[offset++] += input[childOffset++] * ampl;
+					t++;
+					count--;
+				}
+			} else if (t < decayTime) {
+				/* decay */
+				while(count && t < decayTime) {
+					var ampl = attackAmplitude - decayRate * (t - attackTime);
+					buf[offset++] += input[childOffset++] * ampl;
+					buf[offset++] += input[childOffset++] * ampl;
+					t++;
+					count--;
+				}
+			} else {
+				/* sustain */
+				while(count) {
+					buf[offset++] += input[childOffset++] * sustainAmplitude;
+					buf[offset++] += input[childOffset++] * sustainAmplitude;
+					t++;
+					count--;
+				}
+			}
+		}
+	}
+	
+	return self;
+}
+
+function midiToFrequency(note) {
+	return 440 * Math.pow(2, (note-69)/12);
+}
+
+AwesomeProgram = {
+	'createNote': function(note, velocity) {
+		var frequency = midiToFrequency(note);
+		return AwesomeGenerator(frequency);
+	}
+}
+
+PianoProgram = {
+	'attackAmplitude': 0.2,
+	'sustainAmplitude': 0.1,
+	'attackTime': 0.02,
+	'decayTime': 0.3,
+	'releaseTime': 0.02,
+	'createNote': function(note, velocity) {
+		var frequency = midiToFrequency(note);
+		return ADSRGenerator(
+			SineGenerator(frequency),
+			this.attackAmplitude * (velocity / 128), this.sustainAmplitude * (velocity / 128),
+			this.attackTime, this.decayTime, this.releaseTime
+		);
+	}
+}
+
+StringProgram = {
+	'createNote': function(note, velocity) {
+		var frequency = midiToFrequency(note);
+		return ADSRGenerator(
+			SineGenerator(frequency),
+			0.5 * (velocity / 128), 0.2 * (velocity / 128),
+			0.4, 0.8, 0.4
+		);
+	}
+}
+
+PROGRAMS = {
+	41: StringProgram,
+	42: StringProgram,
+	43: StringProgram,
+	44: StringProgram,
+	45: StringProgram,
+	46: StringProgram,
+	47: StringProgram,
+	49: StringProgram,
+	50: StringProgram
+};
+
+function Synth(sampleRate) {
+	
+	var generators = [];
+	
+	function addGenerator(generator) {
+		generators.push(generator);
+	}
+	
+	function generate(samples) {
+		var data = new Array(samples*2);
+		generateIntoBuffer(samples, data, 0);
+		return data;
+	}
+	
+	function generateIntoBuffer(samplesToGenerate, buffer, offset) {
+		for (var i = offset; i < offset + samplesToGenerate * 2; i++) {
+			buffer[i] = 0;
+		}
+		for (var i = generators.length - 1; i >= 0; i--) {
+			generators[i].generate(buffer, offset, samplesToGenerate);
+			if (!generators[i].alive) generators.splice(i, 1);
+		}
+	}
+	
+	return {
+		'sampleRate': sampleRate,
+		'addGenerator': addGenerator,
+		'generate': generate,
+		'generateIntoBuffer': generateIntoBuffer
+	}
+}
